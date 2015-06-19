bdcefcb299fe1e5e833030d291f3e2e0be13c0ef
Joseph Hillhouse <jhillhouse@longviewinc.com>
2015-06-19 16:54:41 -0400

    udpated logic for wordnet

--- MedAnalyzerResources/src/main/java/com/longview/gsa/controller/DrugLabelingController.java
+++ MedAnalyzerResources/src/main/java/com/longview/gsa/controller/DrugLabelingController.java
@@ -4,12 +4,13 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import net.sf.extjwnl.JWNLException;
-import net.sf.extjwnl.data.IndexWord;
+import net.sf.extjwnl.data.IndexWordSet;
 import net.sf.extjwnl.data.POS;
-import net.sf.extjwnl.data.Synset;
 import net.sf.extjwnl.dictionary.Dictionary;
 
 import org.apache.commons.io.IOUtils;
@@ -59,14 +60,37 @@ public class DrugLabelingController {
 	public Greeting cleanWarnings() {
 		try {
 			Dictionary d = 	Dictionary.getDefaultResourceInstance();
-			IndexWord word = d.lookupIndexWord(POS.VERB, "accomplish");
-			List<Synset> wordSynset = word.getSenses();
+			IndexWordSet word = d.lookupAllIndexWords("keep");
+			String nounCount = "There are " + word.getSenseCount(POS.NOUN) + " nouns";
+			String verbCount = "There are " + word.getSenseCount(POS.VERB) + " verbs";
+			String adjectiveCount = "There are " + word.getSenseCount(POS.ADJECTIVE) + " adjectives";
+			String adverbCount = "There are " + word.getSenseCount(POS.ADVERB) + " adverbs";
 			
-			return new Greeting(1, wordSynset.get(0).getGloss());
+			List<Speech> typeOfSpeech = new ArrayList<Speech>();
+			typeOfSpeech.add(new Speech(POS.NOUN, word.getSenseCount(POS.NOUN)));
+			typeOfSpeech.add(new Speech(POS.VERB, word.getSenseCount(POS.VERB)));
+			typeOfSpeech.add(new Speech(POS.ADJECTIVE, word.getSenseCount(POS.NOUN)));
+			typeOfSpeech.add(new Speech(POS.ADVERB, word.getSenseCount(POS.ADVERB)));
+				
+			Collections.sort(typeOfSpeech, (object1, object2) -> object2.count.compareTo(object1.count));
+			
+			if(typeOfSpeech.get(0).pos == POS.NOUN) 
+				return new Greeting(1, "The type of speech is:" + typeOfSpeech.get(0).pos + ". The count is:" + typeOfSpeech.get(0).count);
 		} catch (JWNLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
+	}
+	
+	public class Speech{
+		
+		public POS pos;
+		public Integer count;
+		
+		public Speech(POS pos, Integer count){
+			this.pos = pos;
+			this.count = count;
+		}
 	}
 }
